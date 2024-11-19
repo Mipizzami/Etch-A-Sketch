b3df1966a15a4f6fbb752aa179c20cf0fbc0a1bd
Mipizzami <167435216+Mipizzami@users.noreply.github.com>
2024-11-19 16:47:58 +0100

    end the project

--- javascript.js
+++ javascript.js
@@ -1,7 +1,36 @@
-const container = document.getElementById('grid-container');
+let userInput;
 
-for(let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement('div');
-    square.classList.add('grid-square');
-    container.appendChild(square);
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    userInput = prompt('Please enter size of the grid');
+    console.log(userInput);
+    createGrid(userInput);
+})
+
+function createGrid(size) {
+    const container = document.getElementById('grid-container');
+    container.innerHTML = '';
+
+    const gridSize = parseInt(size);
+    if(gridSize <= 0 || isNaN(gridSize) || gridSize >= 100){
+        alert('Please enter a valid positive number under 100');
+        return;
+    }
+
+    const squareSize = 640 / gridSize;
+
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = 'blue';
+        });
+
+        container.appendChild(square);
+    }
 }
